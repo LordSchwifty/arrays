e3d9c7490368ff4bef46887744d1e6c58943e1de
Tom Doder <tdoder89@gmail.com>
2022-10-18 23:33:46 -0500

    give 3 array methods

--- arrays.js
+++ arrays.js
@@ -4,3 +4,9 @@ var sneakerPrices = [110, 150, 225, 325];
 
 var studiesTooMuch = {true, false, false, true};
 
+students.push("rory")
+// will add another friend "rory " to the end of the array
+sneakerPrices.pop(1)
+// will remove the last element of the array
+studiesTooMuch.unshift(false)
+// will add another element to the beginning of the array and change the array
